9d5b9d56808f760f772bcdd80d8e2fd5e09ab886
Michael <mayou808@learner.dvusd.org>
2024-01-22 12:47:57 -0700

    finish

--- Dex/Form1.Designer.vb
+++ Dex/Form1.Designer.vb
@@ -26,6 +26,7 @@ Partial Class Form1
         Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PreviousToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Field1 = New System.Windows.Forms.TextBox()
         Me.Field2 = New System.Windows.Forms.TextBox()
         Me.Field3 = New System.Windows.Forms.TextBox()
@@ -33,6 +34,10 @@ Partial Class Form1
         Me.Field4 = New System.Windows.Forms.TextBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.firstbutton = New System.Windows.Forms.Button()
+        Me.prevbutton = New System.Windows.Forms.Button()
+        Me.nextbutton = New System.Windows.Forms.Button()
+        Me.lastbutton = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -40,7 +45,7 @@ Partial Class Form1
         'MenuStrip1
         '
         Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.PreviousToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
@@ -65,6 +70,12 @@ Partial Class Form1
         Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
         Me.FileToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
         Me.FileToolStripMenuItem.Text = "Save"
+        '
+        'PreviousToolStripMenuItem
+        '
+        Me.PreviousToolStripMenuItem.Name = "PreviousToolStripMenuItem"
+        Me.PreviousToolStripMenuItem.Size = New System.Drawing.Size(27, 24)
+        Me.PreviousToolStripMenuItem.Text = " "
         '
         'Field1
         '
@@ -115,11 +126,51 @@ Partial Class Form1
         '
         Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
+        'firstbutton
+        '
+        Me.firstbutton.Location = New System.Drawing.Point(170, 409)
+        Me.firstbutton.Name = "firstbutton"
+        Me.firstbutton.Size = New System.Drawing.Size(94, 29)
+        Me.firstbutton.TabIndex = 7
+        Me.firstbutton.Text = "First"
+        Me.firstbutton.UseVisualStyleBackColor = True
+        '
+        'prevbutton
+        '
+        Me.prevbutton.Location = New System.Drawing.Point(270, 409)
+        Me.prevbutton.Name = "prevbutton"
+        Me.prevbutton.Size = New System.Drawing.Size(94, 29)
+        Me.prevbutton.TabIndex = 8
+        Me.prevbutton.Text = "Previous"
+        Me.prevbutton.UseVisualStyleBackColor = True
+        '
+        'nextbutton
+        '
+        Me.nextbutton.Location = New System.Drawing.Point(370, 409)
+        Me.nextbutton.Name = "nextbutton"
+        Me.nextbutton.Size = New System.Drawing.Size(94, 29)
+        Me.nextbutton.TabIndex = 9
+        Me.nextbutton.Text = "Next"
+        Me.nextbutton.UseVisualStyleBackColor = True
+        '
+        'lastbutton
+        '
+        Me.lastbutton.Location = New System.Drawing.Point(470, 409)
+        Me.lastbutton.Name = "lastbutton"
+        Me.lastbutton.Size = New System.Drawing.Size(94, 29)
+        Me.lastbutton.TabIndex = 10
+        Me.lastbutton.Text = "Last"
+        Me.lastbutton.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lastbutton)
+        Me.Controls.Add(Me.nextbutton)
+        Me.Controls.Add(Me.prevbutton)
+        Me.Controls.Add(Me.firstbutton)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Field4)
         Me.Controls.Add(Me.Field5)
@@ -149,4 +200,9 @@ Partial Class Form1
     Friend WithEvents Field4 As TextBox
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents PreviousToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents firstbutton As Button
+    Friend WithEvents prevbutton As Button
+    Friend WithEvents nextbutton As Button
+    Friend WithEvents lastbutton As Button
 End Class
--- Dex/Form1.vb
+++ Dex/Form1.vb
@@ -4,6 +4,8 @@ Imports System.Security.Cryptography.Pkcs
 
 Public Class Form1
     Dim records(50) As String
+    Dim count As Integer
+    Dim current As Integer
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         Field1.Text = ""
         Field2.Text = ""
@@ -22,43 +24,83 @@ Public Class Form1
     End Sub
 
     Private Sub FileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FileToolStripMenuItem.Click
-        Dim outFile As New StreamWriter("Data.txt")
-        outFile.Write(Field1.Text)
-        outFile.Write("|")
-        outFile.Write(Field2.Text)
-        outFile.Write("|")
-        outFile.Write(Field3.Text)
-        outFile.Write("|")
-        outFile.Write(Field4.Text)
-        outFile.Write("|")
-        outFile.Write(Field5.Text)
-        outFile.Write("|")
-        outFile.WriteLine(PictureBox1.ImageLocation)
-        outFile.Close()
+        savetofile()
+    End Sub
+    Sub savetofile()
+        Dim r As String
+        r += Field1.Text
+        r += "|"
+        r += Field2.Text
+        r += "|"
+        r += Field3.Text
+        r += "|"
+        r += Field4.Text
+        r += "|"
+        r += Field5.Text
+        r += "|"
+        r += PictureBox1.ImageLocation
+        If count = 0 Then count = 1
+        records(current) = r
+        Dim outfile As New StreamWriter("data.txt")
+        For index = 0 To count - 1
+            outfile.WriteLine(records(index))
+        Next
+        outfile.Close()
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If IO.File.Exists("data.txt") Then
-            Dim idx As Integer = 0
             Dim inFile As New StreamReader("data.txt")
             While (Not inFile.EndOfStream)
-                records(idx) = inFile.ReadLine
-                idx = idx + 1
+                records(count) = inFile.ReadLine
+                count = count + 1
             End While
             inFile.Close()
             showrecord(0)
         End If
     End Sub
     Public Sub showrecord(index As Integer)
-        Dim fields() As String
-        fields = records(index).Split("|")
-        Field1.Text = fields(0)
-        Field2.Text = fields(1)
-        Field3.Text = fields(2)
-        Field4.Text = fields(3)
-        Field5.Text = fields(4)
-        If File.Exists(fields(5)) Then
-            PictureBox1.Load(fields(5))
+        PictureBox1.Image = Nothing
+        If records(index) <> Nothing Then
+            Dim fields() As String
+            fields = records(index).Split("|")
+            Field1.Text = fields(0)
+            Field2.Text = fields(1)
+            Field3.Text = fields(2)
+            Field4.Text = fields(3)
+            Field5.Text = fields(4)
+            If File.Exists(fields(5)) Then
+                PictureBox1.Load(fields(5))
+            End If
         End If
+    End Sub
+
+    Private Sub firstbutton_Click(sender As Object, e As EventArgs) Handles firstbutton.Click
+        savetofile()
+        current = 0
+        showrecord(current)
+    End Sub
+
+    Private Sub lastbutton_Click(sender As Object, e As EventArgs) Handles lastbutton.Click
+        savetofile()
+        If count > 0 Then
+            current = count - 1
+        End If
+        showrecord(current)
+    End Sub
+    Private Sub prevbutton_Click(sender As Object, e As EventArgs) Handles prevbutton.Click
+        savetofile()
+        If current > 0 Then
+            current = current - 1
+        End If
+        showrecord(current)
+    End Sub
+
+    Private Sub nextbutton_Click(sender As Object, e As EventArgs) Handles nextbutton.Click
+        savetofile()
+        If current < count - 1 Then
+            current = current + 1
+        End If
+        showrecord(current)
     End Sub
 End Class
